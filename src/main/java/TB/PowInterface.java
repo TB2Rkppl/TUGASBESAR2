faf8e582c1f456c3521572dea3635917a117c09f
VENOM-HCK <VENOM-HCK@VENOM-HCK-PC>
2014-06-19 04:11:51 +0700

    menambahkan extend untuk pow

--- src/main/java/TB/PowInterface.java
+++ src/main/java/TB/PowInterface.java
@@ -10,6 +10,10 @@ package TB;
  *
  * @author VENOM-HCK
  */
-public interface PowInterface {
+public interface PowInterface extends MulAndDivInterface {
+    
     public double pow(double n1,double n2);
+    public double mul(double n1,double n2);
+    public double div(double n1,double n2);
+    
 }
